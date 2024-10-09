c4062f9ff004d07160af681368d9f7efb24e5049
igors <igorstsetinets@gmail.com>
2024-10-09 15:17:37 +0300

    [NF] add new functional for UsereEstonia and UserLatvia with new Contract class

---
+++ src/Contract.ts
@@ -0,0 +1,28 @@
+import { User } from './User'
+import { UserEstonia } from './UserEstonia'
+import { UserLatvia } from './UserLatvia'
+
+export class Contract {
+  title: string
+  signed: boolean
+
+  constructor(title: string) {
+    this.title = title
+    this.signed = false
+  }
+  signByEstoniaUser(user: UserEstonia): void {
+    if (user.mobileIDAuthorization) {
+      this.signed = true
+    } else {
+      throw new Error('Cannot sign the contract: Mobile ID authorization is not activated')
+    }
+  }
+
+  signByLatviaUser(user: UserLatvia): void {
+    if (user.activateEParakstsForLatvia) {
+      this.signed = true
+    } else {
+      throw new Error('Cant sign contract: EParaksts is not activated')
+    }
+  }
+}
---
+++ test/testContract.test.ts
@@ -0,0 +1,46 @@
+import { Contract } from '../src/Contract'
+import { UserEstonia } from '../src/UserEstonia'
+import { UserLatvia } from '../src/UserLatvia'
+
+let contract: Contract
+let estonianUser: UserEstonia
+let latvianUser: UserLatvia
+
+beforeEach(() => {
+  contract = new Contract('Test contract')
+  estonianUser = new UserEstonia('Alex', 'Ott', 19, '124', '33333', 'test')
+  latvianUser = new UserLatvia('Alex', 'Ott', 20, '124', '33333', 'test')
+})
+
+describe('Contract signing tests for estonianUser', () => {
+  test('Contract should not be signed by default', () => {
+    expect(contract.signed).toBe(false)
+  })
+  test('Estonian user should be able to sign after Mobile ID authorization', () => {
+    estonianUser.mobileIDAuthorization = true
+    contract.signByEstoniaUser(estonianUser)
+    expect(contract.signed).toBe(true)
+  })
+
+  test('Estonian user cannot sign without Mobile ID authorization', () => {
+    expect(() => {
+      contract.signByEstoniaUser(estonianUser)
+    }).toThrow('Cannot sign the contract: Mobile ID authorization is not activated')
+  })
+})
+
+describe('Contract signing tests for latvianUser', () => {
+  test('Contract should not be signing by default', () => {
+    expect(contract.signed).toBe(false)
+  })
+  test('Latvian user should be able to sign after EParaksts authorization', () => {
+    latvianUser.activateEParakstsForLatvia = true
+    contract.signByLatviaUser(latvianUser)
+    expect(contract.signed).toBe(true)
+  })
+  test('Latvian user cannot sign without EParaksts authorization', () => {
+    expect(() => {
+      contract.signByLatviaUser(latvianUser)
+    }).toThrow('Cant sign contract: EParaksts is not activated')
+  })
+})
